2ab00f1a9a1d1a4fd2559dd27b906b28226c1375
Ymbi <60569456+Tropaul322@users.noreply.github.com>
2020-10-07 16:33:11 +0300

    Fix: stdin for decode

--- ceasar-cipher/caesar-cipher.js
+++ ceasar-cipher/caesar-cipher.js
@@ -41,13 +41,13 @@ const checkInput = (args, flags) => {
       const readStream = fs.createReadStream(`${__dirname}/${args.input}`, "utf8");
       readStream.on("error", (err) => console.error('File with this name doesn\'t exist:' + err))
       readStream.on("data", (chunk) => {
-        checkOutput(args, flags.action === 'encode' ? caesarCipher.caesarCipherEncode(stdin, flags.shift) : caesarCipher.caesarCipherDecode(stdin, flags.shift));
+        checkOutput(args, flags.action === 'encode' ? caesarCipher.caesarCipherEncode(chunk, flags.shift) : caesarCipher.caesarCipherDecode(chunk, flags.shift));
       })
     } else {
       process.stdin.on("readable", () => {
         const stdin = process.stdin.read();
         if (stdin) {
-          checkOutput(args, flags.action === 'encode' ? caesarCipher.caesarCipherEncode(chunk, flags.shift) : caesarCipher.caesarCipherDecode(chunk, flags.shift));
+          checkOutput(args, flags.action === 'encode' ? caesarCipher.caesarCipherEncode(stdin, flags.shift) : caesarCipher.caesarCipherDecode(stdin, flags.shift));
           process.stdin.resume();
         }
       });
